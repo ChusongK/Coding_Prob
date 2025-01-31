b4d5511ee6ec5cf9aa313796c21e74ec922d2bf7
Chansong <quickfailer@gmail.com>
2025-01-31 19:34:26 +0900

    try 1_숫자 카드 2

--- 백준 이분탐색/1_숫자 카드 2.py
+++ 백준 이분탐색/1_숫자 카드 2.py
@@ -9,27 +9,11 @@ M = int(input())
 num = list(map(int, input().split()))
 
 card_dict = {}
-
 for c in card:
     if c in card_dict.keys():
         card_dict[c] += 1
     else:
         card_dict[c] = 1
 
-sorted_card = sorted(card_dict.keys())
-
-for i in num:
-    start = 0
-    end = len(sorted_card)-1
-
-    is_exist = False
-    while start<=end:
-        mid = (start+end)//2
-        if sorted_card[mid]==i:
-            is_exist = True
-            break
-        if sorted_card[mid]<i:
-            start = mid + 1
-        else:
-            end = mid -1
-    print(card_dict[i] if is_exist else 0, end=' ')
+for n in num:
+    print(card_dict[n] if n in card_dict.keys() else 0, end = ' ')
